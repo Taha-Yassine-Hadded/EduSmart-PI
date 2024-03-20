680da46835edc65295b48af62f00023db19c1e3b
unknown <yessinhadded99@gmail.com>
2024-03-20 15:46:40 +0100

    Taha Yassine 20/03/2024

--- migrations/Version20240320144312.php
+++ migrations/Version20240320144312.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240319201807 extends AbstractMigration
+final class Version20240320144312 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,21 @@ final class Version20240319201807 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE password_reset_request (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, reset_code INT DEFAULT NULL, expires_at DATETIME DEFAULT NULL, INDEX IDX_C5D0A95AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE remember_me_token (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, token VARCHAR(255) DEFAULT NULL, expires_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_89FEBAD0A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) DEFAULT NULL, email VARCHAR(255) DEFAULT NULL, password VARCHAR(255) DEFAULT NULL, role VARCHAR(255) DEFAULT NULL, website VARCHAR(255) DEFAULT NULL, classe VARCHAR(255) DEFAULT NULL, pays VARCHAR(255) DEFAULT NULL, localisation VARCHAR(255) DEFAULT NULL, cin VARCHAR(255) DEFAULT NULL, niveau INT DEFAULT NULL, genre VARCHAR(255) DEFAULT NULL, date_naissance DATETIME DEFAULT NULL, profil_picture VARCHAR(255) DEFAULT NULL, prenom VARCHAR(255) DEFAULT NULL, is_enabled TINYINT(1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE password_reset_request ADD CONSTRAINT FK_C5D0A95AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE remember_me_token ADD CONSTRAINT FK_89FEBAD0A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE password_reset_request DROP FOREIGN KEY FK_C5D0A95AA76ED395');
+        $this->addSql('ALTER TABLE remember_me_token DROP FOREIGN KEY FK_89FEBAD0A76ED395');
+        $this->addSql('DROP TABLE password_reset_request');
+        $this->addSql('DROP TABLE remember_me_token');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
     }
