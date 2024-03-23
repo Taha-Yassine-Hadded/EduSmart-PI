29307dacb9e2a4145251e39a5571377797a9b73a
unknown <yessinhadded99@gmail.com>
2024-03-24 00:55:43 +0100

    Sign Up Page

--- migrations/Version20240323225227.php
+++ migrations/Version20240323225227.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240323223546 extends AbstractMigration
+final class Version20240323225227 extends AbstractMigration
 {
     public function getDescription(): string
     {
