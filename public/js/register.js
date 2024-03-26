2f3c57c867e9cbc5bc6899397508f7fbccc29906
unknown <yessinhadded99@gmail.com>
2024-03-27 00:11:21 +0100

    Password Validation

--- public/js/register.js
+++ public/js/register.js
@@ -9,3 +9,36 @@ sign_up_btn.addEventListener('click', () => {
 sign_in_btn.addEventListener('click', () => {
     container.classList.remove("sign-up-mode");
 });
+
+
+document.getElementById('registrationFormTeacher').addEventListener('submit', function(event)  {
+
+    const dateOfBirthInput = document.getElementById('dateTeacher');
+    const selectedDate = new Date(dateOfBirthInput.value);
+    const year = selectedDate.getFullYear();
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
+    if ((currentYear - year) < 16) {
+        event.preventDefault();
+        document.getElementById('errorDateTeacher').classList.remove('d-none');
+    } else {
+        document.getElementById('errorDateTeacher').classList.add('d-none');
+    }
+});
+
+document.getElementById('registrationFormStudent').addEventListener('submit', function(event)  {
+
+    const dateOfBirthInput = document.getElementById('dateStudent');
+    const selectedDate = new Date(dateOfBirthInput.value);
+    const year = selectedDate.getFullYear();
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
+    if ((currentYear - year) < 16) {
+        event.preventDefault();
+        document.getElementById('errorDateStudent').classList.remove('d-none');
+    } else {
+        document.getElementById('errorDateStudent').classList.add('d-none');
+    }
+});
